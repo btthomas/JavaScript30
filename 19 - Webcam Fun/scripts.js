354a4ddf89c92f1114709d92ab8c5cbc829549b7
blake thomas <blaketaylorthomas@gmail.com>
2017-03-14 14:37:07 -0400

    19

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -3,3 +3,91 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+function getVideo() {
+  navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+    .then(mediaStream => {
+      video.src = window.URL.createObjectURL(mediaStream);
+      video.play();
+    })
+    .catch(err => {
+      console.err(err);
+    });
+}
+
+function paint() {
+  const width = video.videoWidth;
+  const height = video.videoHeight;
+
+  canvas.width = width;
+  canvas.height = height;
+
+  return setInterval(() => {
+    ctx.drawImage(video, 0, 0, width, height);
+
+    let pixels = ctx.getImageData(0, 0, width, height);
+    pixels = greenScreen(pixels);
+    ctx.putImageData(pixels, 0, 0);
+
+  }, 20)
+}
+
+function screenshot() {
+  snap.currentTime = 0;
+  snap.play();
+
+  const data = canvas.toDataURL('image/jpeg');
+  const link = document.createElement('a');
+  link.href = data;
+  link.setAttribute('download', 'pic');
+  link.innerHTML = `<img src="${data}" alt="abc">`;
+  strip.insertBefore(link, strip.firstChild);
+}
+
+function redEffect(pixels) {
+  for (let i = 0; i < pixels.data.length; i+=4) {
+    pixels.data[i] = pixels.data[i] + 100;
+    pixels.data[i + 1] = pixels.data[i+1] - 15;
+    pixels.data[i + 2] = pixels.data[i+2] * 0.5;
+  }
+  return pixels;
+}
+
+function rgbSplit(pixels) {
+  for (let i = 0; i < pixels.data.length; i+=4) {
+    pixels.data[i - 150] = pixels.data[i];
+    pixels.data[i - 100] = pixels.data[i+1];
+    pixels.data[i - 300] = pixels.data[i+2];
+  }
+  return pixels;
+}
+
+function greenScreen(pixels) {
+  const levels = {};
+
+  document.querySelectorAll('.rgb input').forEach(input => {
+    levels[input.name] = input.value;
+  })
+
+  for (let i = 0; i < pixels.data.length; i+=4) {
+    red = pixels.data[i];
+    green = pixels.data[i+1];
+    blue = pixels.data[i+2];
+    alpha = pixels.data[i+3];
+
+    if (red >= levels.rmin 
+      && green >= levels.gmin
+      && blue >= levels.bmin
+      && red <= levels.rmax
+      && green <= levels.gmax
+      && blue <= levels.bmax) {
+        pixels.data[i + 3] = 0.01;
+    }
+  }
+  return pixels;
+
+}
+
+getVideo();
+
+video.addEventListener('canplay', paint);
